994067108c536fded553e2a584ef3b50eb8e740a
Kenji Fujiwara <kenji-f@is.naist.jp>
2012-12-13 20:55:43 +0900

    change directory structure

---
+++ src/test/java/jp/naist/sd/kenja/factextractor/test/ASTGitCreatorTest.java
@@ -0,0 +1,14 @@
+package jp.naist.sd.kenja.factextractor.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ASTGitCreatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
