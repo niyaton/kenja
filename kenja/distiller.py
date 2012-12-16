298e5dc67f135ea20f4adc47a92663d0ecfe2ae7
Kenji Fujiwara <kenji-f@is.naist.jp>
2012-12-16 15:24:32 +0900

    distiller一応完成

--- kenja/distiller.py
+++ kenja/distiller.py
@@ -1,4 +1,5 @@
 from git import Repo
+import os
 
 class GitDistiller:
     def __init__(self, git_dir):
@@ -9,21 +10,46 @@ class GitDistiller:
         self.new_repo = self.org_repo.clone(new_repo_path)
 
     def distille(self, new_repo_path, extensions):
+        new_repo_path = os.path.abspath(new_repo_path)
         self.clone_repo(new_repo_path)
         git = self.new_repo.git
-        cmd = ['tree-filter', ','.join(self.create_remove_command(extensions))]
+        execcmd = self.create_remove_command_find(extensions)
+        #execcmd = self.create_remove_command_xargs(extensions)
+        cmd = ['--tree-filter', execcmd]
+
         git.filter_branch(cmd)
 
-    def create_remove_command(self, extensions):
+    def prune_empty(self):
+        git = self.new_repo.git
+        kwargs = {'f':True, 'prune-empty':True}
+        git.filter_branch(**kwargs)
+
+    def create_remove_command_find(self, extensions):
         assert len(extensions) > 0
-        cmd = ['find']
+        cmd = ['find', '.', '\\!', '\\(']
         first_extension = extensions.pop(0)
-        cmd.extend(['-name', '*.%s' % (first_extension)])
+        cmd.extend(['-name', '\'*.%s\'' % (first_extension)])
         for extension in extensions:
-            cmd.extend(['-name', '*.%s' % (extension)])
-        cmd.extend(['-type', 'd'])
+            cmd.extend(['-o', '-name', '\'*.%s\'' % (extension)])
+        cmd.extend(['-o', '-type', 'd'])
+        cmd.append('\\)')
+        cmd.extend(['-exec', 'rm', '{}', '\;'])
+
+        return ' '.join(cmd)
+
+    def create_remove_command_xargs(self, extensions):
+        assert len(extensions) > 0
+        cmd = ['find', '.', '\\!', '\\(']
+        first_extension = extensions.pop(0)
+        cmd.extend(['-name', '\'*.%s\'' % (first_extension)])
+        for extension in extensions:
+            cmd.extend(['-o', '-name', '\'*.%s\'' % (extension)])
+        cmd.extend(['-o', '-type', 'd'])
+        cmd.append('\\)')
+        cmd.extend(['-print0', '|', 'xargs', '-0', 'rm'])
         print cmd
-        return cmd
+        return ' '.join(cmd)
+
 if __name__ == '__main__':
     import argparse
 
