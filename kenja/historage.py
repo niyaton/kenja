75ec73c20094a58a65b5aa84fefff9dff932c6af
Kenji Fujiwara <kenji-f@is.naist.jp>
2013-02-11 22:43:39 +0900

    コードをすっきりさせた．

--- kenja/historage.py
+++ kenja/historage.py
@@ -4,7 +4,6 @@ import re
 from collections import defaultdict
 
 class GitDiffParser:
-
     #header_regex = re.compile(r'^diff --git (a/)+(.*) (b/)+(.*)$')
 
     header_a_blob_regex = re.compile(r'^--- (a/)?(.*)$')
@@ -64,83 +63,56 @@ def is_method_parameters(path):
     return dirname == '[MT]'
 
 def get_class(path):
-    splited_path = path.split('/')
-    cn_index = splited_path.index('[CN]')
-    assert cn_index +1 <= len(splited_path)
-    return splited_path[cn_index + 1]
+    split_path = path.split('/')
+    cn_index = split_path.index('[CN]')
+    assert cn_index +1 <= len(split_path)
+    return split_path[cn_index + 1]
 
 def get_method(path):
-    splited_path = path.split('/')
-    mt_index = splited_path.index('[MT]')
-    assert mt_index +1 <= len(splited_path)
-    return splited_path[mt_index + 1]
+    split_path = path.split('/')
+    mt_index = split_path.index('[MT]')
+    assert mt_index +1 <= len(split_path)
+    return split_path[mt_index + 1]
 
 if __name__ == '__main__':
     historage = Repo('~/kenja_test/columba_distilled/1302090100/base_repo')
-    num_extract_method_candidates = 0
-    num_extract_method_candidates2 = 0
-    num_extracted_method_candidates = 0
-    num_extract_method_candidate_revsions = 0
+
+    extract_method_information = []
+    extract_method_revisions = set()
 
     parser = GitDiffParser()
-
     for commit in historage.iter_commits(historage.head):
-        #print commit.hexsha
         for p in commit.parents:
-            method_added = False
-
-            added_method = []
-            changed_method = []
-            method_changed_class = set()
-            method_added_class = set()
-            extract_method_target_candidates = []
             extracted_method_candidates = defaultdict(list)
-            extracted_method_candidates2 = set()
-            extract_method_pair_candidates = []
 
             diff_index = p.diff(commit, create_patch=True)
 
             for diff in diff_index.iter_change_type('A'):
                 if is_method_body(diff.b_blob.path):
-                    added_method.append(diff.b_blob)
                     method = get_method(diff.b_blob.path)
                     method_name = method[:method.index(r'(')]
-                    #extracted_method_candidates.append(method_name)
                     c = get_class(diff.b_blob.path)
                     extracted_method_candidates[c].append(method_name)
-                    method_added_class.add(c)
 
             for diff in diff_index.iter_change_type('M'):
-                if is_method_body(diff.b_blob.path):
-                    changed_method.append(diff.b_blob)
-                    c = get_class(diff.b_blob.path)
-                    if c not in method_added_class:
-                        continue
-                    (deleted_lines, added_lines) = parser.parse(diff.diff)
-                    if deleted_lines and added_lines:
-                        #method_changed_class.add(c)
-                        m = get_method(diff.b_blob.path)
-                        extract_method_target_candidates.append(m)
-                        for lineno, line in added_lines:
-                            for method in extracted_method_candidates[c]:
-                                if method in line:
-                                    method_changed_class.add(c)
-                                    extract_method_pair_candidates.append((c, m, method, line))
-                                    extracted_method_candidates2.add(method)
-                                    break
+                if not is_method_body(diff.b_blob.path):
+                    continue
+                c = get_class(diff.b_blob.path)
+                if c not in extracted_method_candidates.keys():
+                    continue
+                (deleted_lines, added_lines) = parser.parse(diff.diff)
+                if not(deleted_lines and added_lines):
+                    continue
+                m = get_method(diff.b_blob.path)
+                for method in extracted_method_candidates[c]:
+                    for lineno, line in added_lines:
+                        if method in line:
+                            extract_method_information.append((commit.hexsha, commit.message, c, m, method, line))
+                            extract_method_revisions.add(commit.hexsha)
+                            break # One method call is enough to judge as a candidate
 
-            if added_method and changed_method:
-                num_extract_method_candidates += len(method_changed_class)
-                num_extract_method_candidates2 += len(extract_method_pair_candidates)
-                num_extracted_method_candidates += len(extracted_method_candidates2)
-                if extract_method_pair_candidates:
-                    print commit.hexsha
-                    print 'extracted candidates2:'
-                    for candidate in extract_method_pair_candidates:
-                        print candidate
-                    num_extract_method_candidate_revsions += 1
+    for info in extract_method_information:
+        print info
 
-    print 'candidate classes:', num_extract_method_candidates
-    print 'candidate pairs:', num_extract_method_candidates2
-    print 'extracted candidate method:', num_extracted_method_candidates
-    print 'candidate revisions:', num_extract_method_candidate_revsions
+    print 'candidates:', len(extract_method_information)
+    print 'candidate revisions:', len(extract_method_revisions)
