df566c3a05c15719c8f3c458cbeb19870b2e2f5c
Kenji Fujiwara <niyaton@gmail.com>
2013-03-10 19:53:01 +0900

    テストコードをmainブロックに移動

--- kenja/submodule.py
+++ kenja/submodule.py
@@ -45,49 +45,50 @@ def get_topological_ordered_commits(repo, revs):
                 break
     return topological_sorting(dag)
 
-commits = get_topological_ordered_commits(repo, repo.refs)
-commits.reverse()
+if __name__ == '__main__':
+    commits = get_topological_ordered_commits(repo, repo.refs)
+    commits.reverse()
 
-new_repo = Repo.init('/Users/kenjif/test_git_repo')
+    new_repo = Repo.init('/Users/kenjif/test_git_repo')
 
-submodule_mode = '160000'
-with open('/Users/kenjif/test_gitmodules', 'wb') as f:
-    write_submodule_config(f)
+    submodule_mode = '160000'
+    with open('/Users/kenjif/test_gitmodules', 'wb') as f:
+        write_submodule_config(f)
 
-def create_submodule_tree(odb, submodule_commit_hexsha):
-    submodule_conf = '/Users/kenjif/test_gitmodules'
-    conf_mode, conf_binsha = gittools.write_blob(odb, submodule_conf)
-    tree_contents = []
-    tree_contents.append((conf_mode, conf_binsha, '.gitmodules'))
-    tree_contents.append((submodule_mode, hex_to_bin(submodule_commit_hexsha), 'jEdit'))
+    def create_submodule_tree(odb, submodule_commit_hexsha):
+        submodule_conf = '/Users/kenjif/test_gitmodules'
+        conf_mode, conf_binsha = gittools.write_blob(odb, submodule_conf)
+        tree_contents = []
+        tree_contents.append((conf_mode, conf_binsha, '.gitmodules'))
+        tree_contents.append((submodule_mode, hex_to_bin(submodule_commit_hexsha), 'jEdit'))
 
-    tree_mode, binsha = gittools.mktree_from_iter(odb, tree_contents)
-    return bin_to_hex(binsha)
+        tree_mode, binsha = gittools.mktree_from_iter(odb, tree_contents)
+        return bin_to_hex(binsha)
 
-committed = {}
-tags = {}
-heads = {}
-for tag_ref in repo.tags:
-    tags[tag_ref.commit.hexsha] = tag_ref.name
+    committed = {}
+    tags = {}
+    heads = {}
+    for tag_ref in repo.tags:
+        tags[tag_ref.commit.hexsha] = tag_ref.name
 
-for head in repo.heads:
-    heads[head.commit.hexsha] = head.name
+    for head in repo.heads:
+        heads[head.commit.hexsha] = head.name
 
-for commit_hexsha, num in izip(commits, count()):
-    print num, commit_hexsha
-    git = new_repo.git
-    commit = repo.commit(commit_hexsha)
+    for commit_hexsha, num in izip(commits, count()):
+        print num, commit_hexsha
+        git = new_repo.git
+        commit = repo.commit(commit_hexsha)
 
-    parents = []
-    for parent in commit.parents:
-        parents.append(committed[parent.hexsha])
+        parents = []
+        for parent in commit.parents:
+            parents.append(committed[parent.hexsha])
 
-    message = '[%s] from %s' % (num, commit_hexsha)
-    new_tree = create_submodule_tree(new_repo.odb, commit_hexsha)
-    #new_commit = Commit.create_from_tree(new_repo, tree, message, parents)
-    new_commit = Commit.create_from_tree(new_repo, new_tree, message, parents)
-    if commit_hexsha in tags:
-        new_repo.create_tag(tags[commit_hexsha], ref=new_commit)
-    if commit_hexsha in heads:
-        new_repo.create_head(heads[commit_hexsha], commit=new_commit)
-    committed[commit_hexsha] = new_commit
+        message = '[%s] from %s' % (num, commit_hexsha)
+        new_tree = create_submodule_tree(new_repo.odb, commit_hexsha)
+        #new_commit = Commit.create_from_tree(new_repo, tree, message, parents)
+        new_commit = Commit.create_from_tree(new_repo, new_tree, message, parents)
+        if commit_hexsha in tags:
+            new_repo.create_tag(tags[commit_hexsha], ref=new_commit)
+        if commit_hexsha in heads:
+            new_repo.create_head(heads[commit_hexsha], commit=new_commit)
+        committed[commit_hexsha] = new_commit
