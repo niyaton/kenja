9ce60aa6ab8777a68d6572b514ab08f76347a2bf
Kenji Fujiwara <niyaton@gmail.com>
2013-05-31 09:30:47 +0900

    add parameter to format_for_umldiff

--- refactoring_detection.py
+++ refactoring_detection.py
@@ -3,13 +3,13 @@ from git.repo import Repo
 from kenja.detection.extract_method import detect_extract_method
 import argparse
 
-def format_for_umldiff(a_commit, b_commit, org_commit, a_package, b_package, c, m, method, sim):
-    target_method_info = ['jedit']
+def format_for_umldiff(package_prefix, a_commit, b_commit, org_commit, a_package, b_package, c, m, method, sim):
+    target_method_info = [package_prefix]
     if a_package:
         target_method_info.append(a_package)
     target_method_info.extend((c, m))
     target_method = '.'.join(target_method_info)
-    extracted_method_info = ['jedit']
+    extracted_method_info = [package_prefix]
     if b_package:
         extracted_method_info.append(b_package)
     extracted_method_info.extend((c, method))
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     candidate_revisions = set()
     for a_commit, b_commit, org_commit, a_package, b_package, c, m, method, sim in extract_method_information:
         candidate_revisions.add(b_commit)
-        print format_for_umldiff(a_commit, b_commit, org_commit, a_package, b_package, c, m, method, sim)
+        print format_for_umldiff('jedit', a_commit, b_commit, org_commit, a_package, b_package, c, m, method, sim)
 
     print 'candidates:', len(extract_method_information)
     print 'candidate revisions:', len(candidate_revisions)
