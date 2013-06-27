ae921c6418aaa75081be054df9e7ebff5a00a01e
Kenji Fujiwara <niyaton@gmail.com>
2013-06-27 22:28:22 +0900

    copy implementation from TreeWriter

--- src/main/java/jp/naist/sd/kenja/factextractor/FileFormatTreeWriter.java
+++ src/main/java/jp/naist/sd/kenja/factextractor/FileFormatTreeWriter.java
@@ -1,5 +1,65 @@
 package jp.naist.sd.kenja.factextractor;
 
-public class FileFormatTreeWriter extends TreeWriter {
+import java.io.File;
+import java.io.IOException;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+
+public class FileFormatTreeWriter extends TreeWriter {
+	private File baseDir;
+	private File currentDir;
+	
+	public FileFormatTreeWriter(File baseDir) {
+		this.baseDir = baseDir;
+		if (!baseDir.exists()) {
+			try {
+				Files.createParentDirs(baseDir);
+				baseDir.mkdir();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		currentDir = baseDir;
+	}
+	
+	@Override
+	public void writeTree(Tree tree) {
+		File parentDir = currentDir;
+		if (!tree.isRoot()) {
+			currentDir = new File(currentDir, tree.getName());
+			if (!currentDir.exists())
+				currentDir.mkdir();
+		}
+
+		for (Blob blob : tree.getBlobs()) {
+			writeBlob(blob);
+		}
+
+		for (Tree childTree : tree.getChildTrees()) {
+			writeTree(childTree);
+		}
+		currentDir = parentDir;
+	}	
+	
+	@Override
+	public void writeBlob(Blob blob) {
+		if (!currentDir.exists())
+			try {
+				Files.createParentDirs(currentDir);
+				currentDir.mkdir();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		File blobFile = new File(currentDir, blob.getName());
+		try {
+			blobFile.createNewFile();
+			Files.write(blob.getBody(), blobFile, Charsets.US_ASCII);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
