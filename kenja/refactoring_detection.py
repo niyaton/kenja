b74225611335eae48e98c921adde727ca6e11a95
Kenji Fujiwara <kenji-f@is.naist.jp>
2014-07-18 07:45:40 +0900

    followed PEP8: refactoring_detection.py

--- kenja/refactoring_detection.py
+++ kenja/refactoring_detection.py
@@ -20,10 +20,12 @@ class RefactoringDetectionCommandParser:
         args.func(args)
 
     def add_all_command(self):
-        subparser = self.subparsers.add_parser('all',
-                                               help='detect refactoring from all commits in the Historage')
-        subparser.add_argument('historage_dir',
-                               help='path of Historage dir')
+        help_str = 'detect refactoring from all commits in the Historage'
+        subparser = self.subparsers.add_parser('all', help=help_str)
+
+        help_str = 'path of Historage dir'
+        subparser.add_argument('historage_dir', help=help_str)
+
         subparser.set_defaults(func=self.detect_all)
 
     def detect_all(self, args):
@@ -31,16 +33,19 @@ class RefactoringDetectionCommandParser:
         extract_method_information = detect_extract_method(historage)
 
         candidate_revisions = set()
-        for a_commit, b_commit, org_commit, a_package, b_package, c, m, method, sim in extract_method_information:
-            candidate_revisions.add(b_commit)
-            print self.format_for_umldiff('jedit', a_commit, b_commit, org_commit, a_package, b_package, c, m, method,
-                                          sim)
+        for info in extract_method_information:
+            # info[1]: b_commit
+            candidate_revisions.add(info[1])
+            print self.format_for_umldiff('jedit', *info)
 
         print 'candidates:', len(extract_method_information)
         print 'candidate revisions:', len(candidate_revisions)
 
-    def format_for_umldiff(self, package_prefix, a_commit, b_commit, org_commit, a_package, b_package, c, m, method,
-                           sim):
+    def format_for_umldiff(self, package_prefix,
+                           a_commit, b_commit,
+                           org_commit,
+                           a_package, b_package,
+                           c, m, method, sim):
         target_method_info = [package_prefix]
         if a_package:
             target_method_info.append(a_package)
@@ -51,15 +56,19 @@ class RefactoringDetectionCommandParser:
             extracted_method_info.append(b_package)
         extracted_method_info.extend((c, method))
         extracted_method = '.'.join(extracted_method_info)
-        return '"%s","%s","%s","%s","%s","%s"' % (a_commit, b_commit, org_commit, target_method, extracted_method, sim)
+        return '"%s","%s","%s","%s","%s","%s"' % (a_commit, b_commit,
+                                                  org_commit,
+                                                  target_method, extracted_method, sim)
 
     def add_commits_command(self):
-        subparser = self.subparsers.add_parser('commits',
-                                               help='detect refactoring from commits in the csv')
-        subparser.add_argument('historage_dir',
-                               help='path of Historage dir')
-        subparser.add_argument('commits_list',
-                               help='comma separated commits list. please write a_commit hash and b_commit hash per line')
+        help_str = 'detect refactoring from commits in the csv'
+        subparser = self.subparsers.add_parser('commits', help=help_str)
+
+        help_str = 'path of Historage dir'
+        subparser.add_argument('historage_dir', help=help_str)
+
+        help_str = 'comma separated commits list. please write a_commit hash and b_commit hash per line'
+        subparser.add_argument('commits_list', help=help_str)
         subparser.set_defaults(func=self.detect_from_commits_list)
 
     def detect_from_commits_list(self, args):
@@ -76,9 +85,8 @@ class RefactoringDetectionCommandParser:
         except BadObject, name:
             print "Invalid hash of the commit:", name.message
 
-        for a_commit, b_commit, org_commit, a_package, b_package, c, m, method, sim in extract_method_information:
-            print self.format_for_umldiff('jedit', a_commit, b_commit, org_commit, a_package, b_package, c, m, method,
-                                          sim)
+        for info in extract_method_information:
+            print self.format_for_umldiff('jedit', *info)
 
 
 def main():
@@ -88,4 +96,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
