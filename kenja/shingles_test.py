559bdf424203dab4bd8301934ba4a8c943a53b38
Kenji Fujiwara <kenji-f@is.naist.jp>
2014-07-18 07:33:14 +0900

    followed PEP8: shingles_test.py

--- kenja/shingles_test.py
+++ kenja/shingles_test.py
@@ -5,6 +5,7 @@ from kenja.shingles import tokenizer
 from kenja.shingles import calculate_similarity
 from nose.tools import raises
 
+
 def for_split_to_str(test_data, estimated_words):
     result = split_to_str(test_data)
     assert result.pop(0) == 'code'
@@ -17,8 +18,10 @@ def for_split_to_str(test_data, estimated_words):
     for ew, w in zip(estimated_words, texts):
         assert ew == w
 
+
 def get_shingles_str(shingles):
     return set([(shingle[1], shingle[3]) for shingle in shingles])
+
 
 def check_torknized_str(torknized_result, estimated_result):
     torknized_result = torknized_result[1:]
@@ -26,6 +29,7 @@ def check_torknized_str(torknized_result, estimated_result):
         if result[2] != estimated:
             return False
     return True
+
 
 def test_split_to_str():
     test_data = "public void main()"
@@ -46,28 +50,29 @@ def test_split_to_str():
 
     test_data = "''"
     print "[test]", test_data
-    estimated_result = ["''"] # ''
+    estimated_result = ["''"]  # ''
     for_split_to_str(test_data, estimated_result)
 
     test_data = r"'\"'"
     print "[test]", test_data
-    estimated_result = [r"'\"'"] # '\"'
+    estimated_result = [r"'\"'"]  # '\"'
     for_split_to_str(test_data, estimated_result)
 
     test_data = r'"\'"'
     print "[test]", test_data
-    estimated_result = [r'"\'"'] # '\"'
+    estimated_result = [r'"\'"']  # '\"'
     for_split_to_str(test_data, estimated_result)
 
     test_data = r'"\n"'
     print "[test]", test_data
-    estimated_result = [r'"\n"'] # "\n"
+    estimated_result = [r'"\n"']  # "\n"
     for_split_to_str(test_data, estimated_result)
 
     test_data = r"'\n'"
     print "[test]", test_data
-    estimated_result = [r"'\n'"] # '\n'
+    estimated_result = [r"'\n'"]  # '\n'
     for_split_to_str(test_data, estimated_result)
+
 
 def test_create_two_shingles():
     seq = tokenize(tokenizer, "public void main()")[1:]
@@ -79,6 +84,7 @@ def test_create_two_shingles():
     estimated_result = {('public', 'int'), ('int', 'main'), ('main', '('), ('(', ')')}
     assert result == estimated_result
 
+
 def test_tokenize():
     result = tokenize(tokenizer, "public void main()")
     estimated_result = ['public', 'void', 'main', '(', ')']
@@ -87,11 +93,13 @@ def test_tokenize():
     result = tokenize(tokenizer, r'String str="\""')
     assert check_torknized_str(result, estimated_result)
 
+
 def test_calculate_similarity():
     script1 = "public void main()"
     script2 = "public int main()"
     result = calculate_similarity(script1, script2)
     assert result == (1.0 / 3.0)
+
 
 @raises(ZeroDivisionError)
 def test_calculate_similarity():
