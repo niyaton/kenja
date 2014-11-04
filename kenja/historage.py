b8f4e942083a79a23f90c103ad9c3fa3084feed3
Kenji Fujiwara <niyaton@gmail.com>
2014-11-04 12:54:42 +0900

    tmp commit

--- kenja/historage.py
+++ kenja/historage.py
@@ -9,6 +9,13 @@ def is_method_body(path):
     return dirname == '[MT]'
 
 
+def is_constructor_body(path):
+    if os.path.basename(path) != 'body':
+        return False
+    dirname = os.path.basename(os.path.dirname(os.path.dirname(path)))
+    return dirname == '[CS]'
+
+
 def is_method_parameters(path):
     if os.path.basename(path) != 'parameters':
         return False
@@ -16,16 +23,31 @@ def is_method_parameters(path):
     return dirname == '[MT]'
 
 
+def is_constructor_parameters(path):
+    if os.path.basename(path) != 'parameters':
+        return False
+    dirname = os.path.basename(os.path.dirname(os.path.dirname(path)))
+    return dirname == '[CS]'
+
+
 def get_class(path):
     split_path = path.split('/')
+    split_path.reverse()
     cn_index = split_path.index('[CN]')
-    assert cn_index + 1 <= len(split_path)
-    return split_path[cn_index + 1]
+    assert cn_index - 1 <= len(split_path)
+    return split_path[cn_index - 1]
 
 
 def get_method(path):
     split_path = path.split('/')
     mt_index = split_path.index('[MT]')
+    assert mt_index + 1 <= len(split_path)
+    return split_path[mt_index + 1]
+
+
+def get_constructor(path):
+    split_path = path.split('/')
+    mt_index = split_path.index('[CS]')
     assert mt_index + 1 <= len(split_path)
     return split_path[mt_index + 1]
 
