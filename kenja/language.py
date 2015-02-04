6f7b64bb21768565b7443301559c606a094bdd38
Kenji Fujiwara <kenji-f@is.naist.jp>
2015-02-05 08:50:18 +0900

    use key of extension_dict instead of supported_language variable

--- kenja/language.py
+++ kenja/language.py
@@ -1,14 +1,12 @@
-supported_languages = ['java']
-
 extension_dict = {'java': ['.java']}
+
 
 def is_target_blob(blob):
     if not blob:
         return False
 
-    for language in supported_languages:
-        if language in extension_dict:
-            for ext in extension_dict[language]:
-                if blob.name.endswith(ext):
-                    return True
+    for exts in extension_dict.values():
+        for ext in exts:
+            if blob.name.endswith(ext):
+                return True
     return False
