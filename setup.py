a97c0c27f38f3a0632caddb0c6813170b38251e1
Kenji Fujiwara <kenji-f@is.naist.jp>
2015-02-11 03:00:15 +0900

    extracted copy_java_parser function

--- setup.py
+++ setup.py
@@ -8,31 +8,34 @@ from setuptools import setup, find_packages
 
 kenja_version = '0.6-122-gbd1964f'
 
-parser_path = 'kenja/lib/java/java-parser.jar'
-parser_location = 'https://github.com/niyaton/kenja-java-parser/releases/download/0.3/kenja-java-parser-0.3-jar-with-dependencies.jar'
-parser_digest = '9e45f37dd7f52f5cf5c817026159db3b'
+def copy_java_parser():
+    parser_path = 'kenja/lib/java/java-parser.jar'
+    parser_location = 'https://github.com/niyaton/kenja-java-parser/releases/download/0.3/kenja-java-parser-0.3-jar-with-dependencies.jar'
+    parser_digest = '9e45f37dd7f52f5cf5c817026159db3b'
 
-confirm_text = None
-exit_when_no = True
-if not os.path.exists(parser_path):
-    confirm_text = "{0} does not exist. Do you want to download it?[y/n]".format(parser_path)
-elif hashlib.md5(open(parser_path).read()).hexdigest() != parser_digest:
-    confirm_text = "{0} is different from designated parser script. Do you want to overwrite it?[y/n]".format(parser_path)
-    exit_when_no = False
+    confirm_text = None
+    exit_when_no = True
+    if not os.path.exists(parser_path):
+        confirm_text = "{0} does not exist. Do you want to download it?[y/n]".format(parser_path)
+    elif hashlib.md5(open(parser_path).read()).hexdigest() != parser_digest:
+        confirm_text = "{0} is different from designated parser script. Do you want to overwrite it?[y/n]".format(parser_path)
+        exit_when_no = False
 
-if confirm_text is not None:
-    print(confirm_text)
-    choice = raw_input().lower()
-    yes = set(['yes', 'y', 'ye'])
-    no = set(['no', 'n'])
-    if choice in yes:
-        urllib.urlretrieve(parser_location, parser_path)
-        digest = hashlib.md5(open(parser_path).read()).hexdigest()
-        if parser_digest != digest:
-            print("md5 hash of {0} is incorrect! remove it and try again.".format(parser_path))
+    if confirm_text is not None:
+        print(confirm_text)
+        choice = raw_input().lower()
+        yes = set(['yes', 'y', 'ye'])
+        no = set(['no', 'n'])
+        if choice in yes:
+            urllib.urlretrieve(parser_location, parser_path)
+            digest = hashlib.md5(open(parser_path).read()).hexdigest()
+            if parser_digest != digest:
+                print("md5 hash of {0} is incorrect! remove it and try again.".format(parser_path))
+                sys.exit(1)
+        elif exit_when_no:
             sys.exit(1)
-    elif exit_when_no:
-        sys.exit(1)
+
+copy_java_parser()
 
 try:
     kenja_version = subprocess.check_output(["git", "describe"]).rstrip()
